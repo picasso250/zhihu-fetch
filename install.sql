b3f4b8cb2e896260757821932b0d989c03182a1c
wangxiaochi <wangxiaochi@baidu.com>
2015-07-09 19:21:00 +0800

    view

--- install.sql
+++ install.sql
@@ -1,13 +1,27 @@
-CREATE TABLE `zhihu`.`to_be` (
-  `id` INT NOT NULL,
-  `url` VARCHAR(255) NOT NULL,
-  `create_time` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id`));
-ALTER TABLE `zhihu`.`to_be` 
-ADD UNIQUE INDEX `idx_url` (`url` ASC);
-ALTER TABLE `zhihu`.`to_be` 
-ADD COLUMN `fetched` TINYINT UNSIGNED NOT NULL AFTER `create_time`;
-ALTER TABLE `zhihu`.`to_be` 
-CHANGE COLUMN `fetched` `fetched` TINYINT(3) UNSIGNED NOT NULL AFTER `url`;
-ALTER TABLE `zhihu`.`to_be` 
-CHANGE COLUMN `id` `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ;
+CREATE TABLE `task` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `fetched` tinyint(3) unsigned NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_url` (`url`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `question` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `detail` text,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fetch` tinyint(4) NOT NULL DEFAULT '0',
+  `follow_count` int(10) unsigned NOT NULL,
+  `answer_count` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_title` (`title`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `answer` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `qid` int(10) unsigned DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_qu` (`qid`,`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
